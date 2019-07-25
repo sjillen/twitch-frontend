83d96ccf20541bc04f9980e6098ad66fc0ced233
Thomas Proust <thomas.proust89@gmail.com>
2019-07-25 09:47:00 +0200

    update server.js file

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/ubisoft'));
 
-/*app.get('/*', function(req, res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/ubisoft/index.html'));
-});*/
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
